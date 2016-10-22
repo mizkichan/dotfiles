92e48a49b28d5ad325692de7267c098cccde18be
Mizki SUZUMORI <mizki9577@gmail.com>
2016-10-22 23:42:42 +0900

    foo

--- .i3/power.sh
+++ .i3/power.sh
@@ -1,12 +1,38 @@
 #!/bin/sh
-sh -c "$(
-    zenity --title Exit --width 320 --height 240 \
-           --list --text= --column= --hide-column 1 --hide-header \
-           --column= \
-           "i3-msg restart" "Restart i3" \
-           "i3-msg exit" "Logout" \
-           "systemctl reboot" "Reboot" \
-           "systemctl poweroff" "Shutdown" \
-           "systemctl suspend" "Suspend" \
-           "slock & systemctl hibernate" "Hibernate" \
-           "mpv --fs --mute --loop --start 15.85 --ab-loop-a 15.85 --ab-loop-b 22.24 $HOME/video/panda_office.mkv" "Panda!!!")"
+result=$(dmenu -l 20 <<EOI
+
+hibernate
+lock
+logout
+poweroff
+reboot
+restart
+shutdown
+suspend
+boot windows
+EOI
+)
+
+case $result in
+	lock)
+		swaylock
+		;;
+	logout)
+		swaymsg exit
+		;;
+	restart)
+		swaymsg restart
+		;;
+	hibernate | suspend)
+		slock systemctl $result
+		;;
+	"boot windows")
+		sudo -A efibootmgr -n 0000
+		[ $? -eq 0 ] && systemctl reboot
+		;;
+	"")
+		;;
+	*)
+		systemctl $result
+		;;
+esac
--- .zshenv
+++ .zshenv
@@ -3,3 +3,4 @@ PATH=$PATH:$HOME/.gem/ruby/2.3.0/bin	# RubyGems も ~/.local/ に入れられな
 export PATH
 export NODE_PATH=$HOME/.local/lib/node_modules
 export EDITOR=vim
+[ -z "$DISPLAY" -a "$(fgconsole)" -eq 1 ] && exec startx
